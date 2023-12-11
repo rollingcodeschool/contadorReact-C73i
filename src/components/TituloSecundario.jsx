a97fb08275ffb3fca3d543d71bb60f3dab77f09a
Emilse Arias <emilsearias@gmail.com>
2023-12-11 11:17:20 -0300

    desestructuramos el objeto props en el componente TituloSecundario

--- src/components/TituloSecundario.jsx
+++ src/components/TituloSecundario.jsx
@@ -1,11 +1,11 @@
-const TituloSecundario = (props) => {
+const TituloSecundario = ({comision, anioActualProps}) => {
     return (
         <article>
-            <h2>Practica con react {props.anioActualProps}</h2>
+            <h2>Practica con react {anioActualProps}</h2>
             <p>
             React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario interactivas y eficientes en aplicaciones web de una sola página. Su enfoque declarativo y su arquitectura basada en componentes facilitan la creación de interfaces reutilizables y escalables. React ha ganado popularidad debido a su rendimiento optimizado y su capacidad para trabajar de manera integrada con otras tecnologías, convirtiéndolo en una herramienta fundamental en el desarrollo moderno de aplicaciones web.
             <br/>
-            Comision: {props.comision}
+            Comision: {comision}
             </p>
         </article>
     );
