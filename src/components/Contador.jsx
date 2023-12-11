80fd8fdf296b00b1f38e9b49652bcf68adf7cb35
Emilse Arias <emilsearias@gmail.com>
2023-12-11 11:42:27 -0300

    agregamos la funcion para el onClick del botón +1 en el componente Contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,11 +1,21 @@
 const Contador = () => {
-    return (
-        <article className="text-center">
-            <h2>Contador</h2>
-            <h3>10</h3>
-            <button className="btn btn-primary">+1</button>
-        </article>
-    );
+  let numero = 10;
+
+  return (
+    <article className="text-center">
+      <h2>Contador</h2>
+      <h3>{numero}</h3>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          numero++;
+          console.log(numero);
+        }}
+      >
+        +1
+      </button>
+    </article>
+  );
 };
 
 export default Contador;
