1255b34e24b05fc3e20c249286780d127108349d
Emilse Arias <emilsearias@gmail.com>
2023-12-13 10:14:26 -0300

    agregamos un estado al componente Contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,17 +1,14 @@
-const Contador = () => {
-  let numero = 10;
+import { useState } from "react";
 
+const Contador = () => {
+  const [numero, setNumero] = useState(10);
+  // let numero = 10;
+  // numero++ -> numero = numero +1
   return (
     <article className="text-center">
       <h2>Contador</h2>
       <h3>{numero}</h3>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          numero++;
-          console.log(numero);
-        }}
-      >
+      <button className="btn btn-primary" onClick={() => setNumero(numero + 1)}>
         +1
       </button>
     </article>
